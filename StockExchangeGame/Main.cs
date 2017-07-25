380e2065ed828ed3113595d1041fb1c10b0b8849
tim <jan-brenner@gmx.de>
2017-07-25 23:03:02 +0200

    Initial source commit.

---
+++ StockExchangeGame/Main.cs
@@ -0,0 +1,12 @@
+﻿using System.Windows.Forms;
+
+namespace StockExchangeGame
+{
+    public partial class Main : Form
+    {
+        public Main()
+        {
+            InitializeComponent();
+        }
+    }
+}
