75033a00a0d59798719f298a85ab66671397c9aa
tim <jan-brenner@gmx.de>
2017-07-26 18:20:51 +0200

    Added dummy combobox and label.

--- StockExchangeGame/Main.Designer.cs
+++ StockExchangeGame/Main.Designer.cs
@@ -29,21 +29,48 @@
         private void InitializeComponent()
         {
             System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Main));
+            this.comboBoxLanguage = new System.Windows.Forms.ComboBox();
+            this.labelSelectLanguage = new System.Windows.Forms.Label();
             this.SuspendLayout();
+            // 
+            // comboBoxLanguage
+            // 
+            this.comboBoxLanguage.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.comboBoxLanguage.FormattingEnabled = true;
+            this.comboBoxLanguage.Location = new System.Drawing.Point(45, 34);
+            this.comboBoxLanguage.Name = "comboBoxLanguage";
+            this.comboBoxLanguage.Size = new System.Drawing.Size(121, 21);
+            this.comboBoxLanguage.TabIndex = 0;
+            this.comboBoxLanguage.SelectedIndexChanged += new System.EventHandler(this.comboBoxLanguage_SelectedIndexChanged);
+            // 
+            // labelSelectLanguage
+            // 
+            this.labelSelectLanguage.AutoSize = true;
+            this.labelSelectLanguage.Location = new System.Drawing.Point(42, 74);
+            this.labelSelectLanguage.Name = "labelSelectLanguage";
+            this.labelSelectLanguage.Size = new System.Drawing.Size(107, 13);
+            this.labelSelectLanguage.TabIndex = 1;
+            this.labelSelectLanguage.Text = "labelSelectLanguage";
             // 
             // Main
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(284, 261);
+            this.Controls.Add(this.labelSelectLanguage);
+            this.Controls.Add(this.comboBoxLanguage);
             this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
             this.Name = "Main";
             this.Text = "Form1";
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
+
+        private System.Windows.Forms.ComboBox comboBoxLanguage;
+        private System.Windows.Forms.Label labelSelectLanguage;
     }
 }
 
