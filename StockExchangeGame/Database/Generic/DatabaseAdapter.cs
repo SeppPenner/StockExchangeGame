4bc3bc190a277b329554aaa63b3fa8e2561e9438
tim <jan-brenner@gmx.de>
2017-07-26 17:31:18 +0200

    Updated DatabaseAdapter and corresponding interface.

--- StockExchangeGame/Database/Generic/DatabaseAdapter.cs
+++ StockExchangeGame/Database/Generic/DatabaseAdapter.cs
@@ -10,7 +10,7 @@ namespace StockExchangeGame.Database.Generic
     {
         private const string SqlDbFileName = "StockGame.db3";
 
-        private string GetDatabasePath()
+        public string GetDatabasePath()
         {
             var location = Assembly.GetExecutingAssembly().Location;
             return location != null ? Path.Combine(Directory.GetParent(location).FullName, SqlDbFileName) : string.Empty;
@@ -21,6 +21,41 @@ namespace StockExchangeGame.Database.Generic
             return new SQLiteAsyncConnection(GetDatabasePath());
         }
 
+        public async Task<CreateTablesResult> CreateBoughtTable()
+        {
+            return await GetConnection().CreateTableAsync<Bought>();
+        }
+
+        public async Task<CreateTablesResult> CreateCompanyEndingsTable()
+        {
+            return await GetConnection().CreateTableAsync<CompanyEndings>();
+        }
+
+        public async Task<CreateTablesResult> CreateCompanyNamesTable()
+        {
+            return await GetConnection().CreateTableAsync<CompanyNames>();
+        }
+
+        public async Task<CreateTablesResult> CreateDummyCompanyTable()
+        {
+            return await GetConnection().CreateTableAsync<DummyCompany>();
+        }
+
+        public async Task<CreateTablesResult> CreateMerchantTable()
+        {
+            return await GetConnection().CreateTableAsync<Merchant>();
+        }
+
+        public async Task<CreateTablesResult> CreateNamesTable()
+        {
+            return await GetConnection().CreateTableAsync<Names>();
+        }
+
+        public async Task<CreateTablesResult> CreateSoldTable()
+        {
+            return await GetConnection().CreateTableAsync<Sold>();
+        }
+
         public async Task<CreateTablesResult> CreateStockTable()
         {
             return await GetConnection().CreateTableAsync<Stock>();
@@ -29,6 +64,21 @@ namespace StockExchangeGame.Database.Generic
         public async Task<CreateTablesResult> CreateStockHistoryTable()
         {
             return await GetConnection().CreateTableAsync<StockHistory>();
+        }
+
+        public async Task<CreateTablesResult> CreateStockMarketTable()
+        {
+            return await GetConnection().CreateTableAsync<StockMarket>();
+        }
+
+        public async Task<CreateTablesResult> CreateSurnamesTable()
+        {
+            return await GetConnection().CreateTableAsync<Surnames>();
+        }
+
+        public async Task<CreateTablesResult> CreateTaxesTable()
+        {
+            return await GetConnection().CreateTableAsync<Taxes>();
         }
     }
 }
--- StockExchangeGame/Database/Generic/IDatabaseAdapter.cs
+++ StockExchangeGame/Database/Generic/IDatabaseAdapter.cs
@@ -4,14 +4,46 @@ namespace StockExchangeGame.Database.Generic
 {
     public interface IDatabaseAdapter
     {
-        // ReSharper disable once UnusedMember.Global
+        // ReSharper disable once UnusedMemberInSuper.Global
+        string GetDatabasePath();
+
         // ReSharper disable once UnusedMemberInSuper.Global
         SQLiteAsyncConnection GetConnection();
+
+        // ReSharper disable once UnusedMember.Global
+        Task<CreateTablesResult> CreateBoughtTable();
+
+        // ReSharper disable once UnusedMember.Global
+        Task<CreateTablesResult> CreateCompanyEndingsTable();
+
+        // ReSharper disable once UnusedMember.Global
+        Task<CreateTablesResult> CreateCompanyNamesTable();
+
+        // ReSharper disable once UnusedMember.Global
+        Task<CreateTablesResult> CreateDummyCompanyTable();
+
+        // ReSharper disable once UnusedMember.Global
+        Task<CreateTablesResult> CreateMerchantTable();
+
+        // ReSharper disable once UnusedMember.Global
+        Task<CreateTablesResult> CreateNamesTable();
+
+        // ReSharper disable once UnusedMember.Global
+        Task<CreateTablesResult> CreateSoldTable();
 
         // ReSharper disable once UnusedMember.Global
         Task<CreateTablesResult> CreateStockTable();
 
         // ReSharper disable once UnusedMember.Global
         Task<CreateTablesResult> CreateStockHistoryTable();
+
+        // ReSharper disable once UnusedMember.Global
+        Task<CreateTablesResult> CreateStockMarketTable();
+
+        // ReSharper disable once UnusedMember.Global
+        Task<CreateTablesResult> CreateSurnamesTable();
+
+        // ReSharper disable once UnusedMember.Global
+        Task<CreateTablesResult> CreateTaxesTable();
     }
 }
