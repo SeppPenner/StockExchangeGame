de62bace99b672e94f59a1fb1490476ce85b1e80
tim <jan-brenner@gmx.de>
2017-07-27 12:24:21 +0200

    Added correct SQLite packages.

--- StockExchangeGame/Properties/Resources.Designer.cs
+++ StockExchangeGame/Properties/Resources.Designer.cs
@@ -44,7 +44,7 @@ namespace StockExchangeGame.Properties
             {
                 if ((resourceMan == null))
                 {
-                    global::System.Resources.ResourceManager temp = new global::System.Resources.ResourceManager("StockExchangeGame.Properties.Resources", typeof(Resources).Assembly);
+                    var temp = new global::System.Resources.ResourceManager("StockExchangeGame.Properties.Resources", typeof(Resources).Assembly);
                     resourceMan = temp;
                 }
                 return resourceMan;
