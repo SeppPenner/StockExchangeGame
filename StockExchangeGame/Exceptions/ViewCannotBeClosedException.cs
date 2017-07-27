e7309467d1f1abe59c0350763d32f363fd85551d
tim <jan-brenner@gmx.de>
2017-07-27 12:34:20 +0200

    Added ViewCannotBeClosedException.

---
+++ StockExchangeGame/Exceptions/ViewCannotBeClosedException.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace StockExchangeGame.Exceptions
+{
+    // ReSharper disable once UnusedMember.Global
+    public class ViewCannotBeClosedException : Exception
+    {
+        public ViewCannotBeClosedException()
+        {
+        }
+
+        // ReSharper disable once UnusedMember.Global
+        public ViewCannotBeClosedException(string message)
+            : base(message)
+        {
+        }
+
+        // ReSharper disable once UnusedMember.Global
+        public ViewCannotBeClosedException(string message, Exception inner)
+            : base(message, inner)
+        {
+        }
+    }
+}
