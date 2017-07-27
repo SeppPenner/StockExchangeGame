66d3261f264a431f5a60b5700c24accfa45ec4b3
tim <jan-brenner@gmx.de>
2017-07-27 12:47:23 +0200

    Added attributes.

---
+++ StockExchangeGame/Database/Attributes/AutoIncrementAttribute.cs
@@ -0,0 +1,10 @@
+﻿using System;
+
+namespace StockExchangeGame.Database.Attributes
+{
+    [AttributeUsage(AttributeTargets.Property)]
+    // ReSharper disable once UnusedMember.Global
+    public class AutoIncrementAttribute : Attribute
+    {
+    }
+}
---
+++ StockExchangeGame/Database/Attributes/PrimaryKeyAttribute.cs
@@ -0,0 +1,9 @@
+﻿using System;
+
+namespace StockExchangeGame.Database.Attributes
+{
+    [AttributeUsage(AttributeTargets.Property)]
+    public class PrimaryKeyAttribute : Attribute
+    {
+    }
+}
